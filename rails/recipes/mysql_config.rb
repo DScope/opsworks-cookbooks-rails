7c279cbe3ccce8efb45a197b2d1c84ef7dc5ddb1
Antonio Grass <antoniograssb@gmail.com>
2017-12-07 18:09:20 -0200

    add directory if not exists

--- rails/recipes/mysql_config.rb
+++ rails/recipes/mysql_config.rb
@@ -1,6 +1,12 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
   puts "Inflating template database.yml.erb"
+  directory "#{deploy[:deploy_to]}/shared/config" do
+      group 'root'
+      owner "#{deploy[:user]}"
+      mode '0755'
+      action :create
+  end
   template "#{deploy[:deploy_to]}/shared/config/database.yml" do
     source "database.yml.erb"
     cookbook "rails"
