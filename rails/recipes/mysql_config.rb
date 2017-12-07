83b86835e87b659d6422903c45935e0194ce2f6d
Antonio Grass <antoniograssb@gmail.com>
2017-12-07 18:17:19 -0200

    add dir

--- rails/recipes/mysql_config.rb
+++ rails/recipes/mysql_config.rb
@@ -1,6 +1,12 @@
 node[:deploy].each do |application, deploy|
   deploy = node[:deploy][application]
   puts "Inflating template database.yml.erb"
+  directory "#{deploy[:deploy_to]}/shared" do
+      group 'root'
+      owner "#{deploy[:user]}"
+      mode '0755'
+      action :create
+  end
   directory "#{deploy[:deploy_to]}/shared/config" do
       group 'root'
       owner "#{deploy[:user]}"
